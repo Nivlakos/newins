da1f1ae86f0a5cd433f77df72b1266ec5ef99ff0
alutchenkov <alutchenkov@gmail.com>
2025-01-09 12:20:35 +0300

    current progress. still to do: 1) chrome headless mode support; 2) db access code update; 3) split parsing to 2 microservices interacting via Redis, needed to increase stability

---
+++ completed-tender-parse/app/config.py
@@ -0,0 +1,12 @@
+# Tender database connection string
+TENDER_DB_SERVER = 'localhost'
+TENDER_DB_PORT = 5432
+TENDER_DB_USER = 'postgres'
+TENDER_DB_PASS = '_StrongPass1'
+TENDER_DB_NAME = 'tender_data'
+
+# Parser configuration
+ROSTENDER_USER = 's3m3np0p'
+ROSTENDER_PASS = 'CDTWdhip'
+CHROME_USER_DATA_FOLDER = '/Users/andrel/Library/Application Support/Google/Chrome/'
+CHROME_PROFILE_NAME = 'Profile 2' # Chrome profile to use (to skip login if already logged in)
